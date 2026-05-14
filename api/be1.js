0ade88c6d893c186dee3605b4a4a195615185401
kimsubsub <gustjqv12@naver.com>
2026-05-14 23:28:19 +0900

    fix: Vercel API 프록시 rewrite 보강

---
+++ api/be1.js
@@ -0,0 +1,8 @@
+const { proxyRequest } = require('./_proxy');
+
+module.exports = function handler(request, response) {
+  return proxyRequest(request, response, {
+    envName: 'BE1_PROXY_TARGET_URL',
+    pathQueryParam: 'path',
+  });
+};
